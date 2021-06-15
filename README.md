dcb2f55d43561b0cc60efe9e815abcd3364fbb29
Bradford-Meziere <coleokc@gmail.com>
2021-06-14 21:44:14 -0500

    Filled out README file

--- README.md
+++ README.md
@@ -43,6 +43,9 @@ Find the file `index.js` and complete the tasks.
 Edit the `ReadMe` file with your answers.
 
 1. In your own words, define closure (1-2 sentences).
+
+Closure is when you are grabbing a variable from outside the child function.
+
 2. Study the following code, then answer the questions below.
 
 ```js
@@ -65,12 +68,16 @@ dansRoll();
 
 a. Where is closure used in this code? How can you tell?
 
+The Closure would be
 
 b. Compare and contrast calling `dansRoll` the first and second time What is always the same? What could change?
+
+every time you call dansRoll, you will receive Dan rolled a (roll). Everytime its invoked the newRoll will be different.
 
 
 c. What is the lexical scope of `newRoll`? 
 
+The scope is personalDice
 
 ### Task 3 - Stretch Goals
 
--- index.js
+++ index.js
@@ -105,14 +105,14 @@ Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the score function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore() {
-  const scoreByInning {
-      Home: scoreCB(),
-      Away: scoreCB()
- } 
-  return (`Home : ${scoreByInning.Home} Away : ${scoreByInning.Away}`)
-}
-  console.log
+// function getInningScore() {
+//   const scoreByInning {
+//       Home: scoreCB(),
+//       Away: scoreCB()
+ 
+//   return (`Home : ${scoreByInning.Home} Away : ${scoreByInning.Away}`)
+//   }
+//   console.log(getInningScore)
 
 /* ⚾️⚾️⚾️ Task 5: scoreboard() ⚾️⚾️⚾️
 Use the scoreboard function below to do the following:
